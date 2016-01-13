16cc5328ac6d4691d3c48f2def055874081757d6
David Tin Nyo <tinman@robotseamonsters.com>
2016-01-13 12:24:20 -0800

    use latest versions of deps

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.5.0"
-  :source-paths ["src/clj" "src/cljs"]
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/clojurescript "0.0-3291" :scope "provided"]
-                 [compojure "1.3.4"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [ring/ring-devel "1.3.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-devel "1.4.0"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler vimcljsrepl.handler/app}
+  :source-paths ["src/clj" "src/cljs"]
   :profiles
   {:dev {:repl-options {:init-ns vimcljsrepl.handler
                         :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
